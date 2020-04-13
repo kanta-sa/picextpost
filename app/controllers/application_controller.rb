13a30607a5c6362536d7ad5c1664f936976dd394
kantaro.sasajima <h27s10k.s@gmail.com>
2020-04-13 07:04:18 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  
+  private #アクションではない関数を以下に定義する
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
