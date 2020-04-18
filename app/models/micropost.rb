f4311320704114c64316230f873d52257fcba0e7
kantaro.sasajima <h27s10k.s@gmail.com>
2020-04-18 04:43:35 +0000

    upload picture for micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,6 @@ class Micropost < ApplicationRecord
   #favorite機能の実装について
   has_many :favorites, dependent: :destroy
   has_many :favorite_of_users, through: :favorites, source: :user
+  
+  mount_uploader :image, ImageUploader
 end
